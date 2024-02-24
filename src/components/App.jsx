20daa2ed1693eef7c811b7e8c78fe822507cadfe
Vineet <maddheshiyavineet7800@gmail.com>
2024-02-24 21:21:33 +0530

    Add/Delete Note Updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    // console.log(note);
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote]
+    })
+  }
+
+  function deleteNote(id) {
+    // console.log("Delete was triggered!");
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {  
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return <Note 
+          key= {index}
+          id= {index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+        />
+      })}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
       <Footer />
     </div>
   );
